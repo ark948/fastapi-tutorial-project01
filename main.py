04deefc76eddd3aa7d2f128b737649a8a2b5d390
ark948 <armankarimi948@gmail.com>
2024-07-17 20:52:53 +0330

    request body + path + query parameter in one route

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -12,6 +19,28 @@ app = FastAPI()
 async def root():
     return {"message": "Hello world"}
 
+# request body
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# request body + path parameter
+# ** to unpack the variable
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+# request body + path + query parameter
+@app.put("/items/{item_id}")
+async def upadte_item2(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
