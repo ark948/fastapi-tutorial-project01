b1e3431244ad43e36ed6c34df104a8f0685bd6f6
ark948 <armankarimi948@gmail.com>
2024-07-17 17:15:26 +0330

    new path with parameter

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
