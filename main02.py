e20d6b28125bf5b8047ffac849b64baecc3c3685
ark948 <armankarimi948@gmail.com>
2024-07-17 22:50:55 +0330

    more query parameter stuff

--- main02.py
+++ main02.py
@@ -14,7 +14,7 @@ async def read_item(q: Annotated[str | None, Query(max_length=5)] = None):
     return results
 
 @app.get("/items02/")
-async def read_item_with_required_q_ellipsis(q: Annotated[str, Query(min_length=3)] = ...):
+async def read_items_with_required_q_ellipsis(q: Annotated[str, Query(min_length=3)] = ...):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
@@ -22,3 +22,24 @@ async def read_item_with_required_q_ellipsis(q: Annotated[str, Query(min_length=
 
 # older version
 # async def read_items(q: str | None = Query(default=None, max_length=50)):
+
+@app.get("/items03/")
+async def read_items_multiple_query_values(
+    q: Annotated[list[str] | None, 
+        Query(
+        "foo", "bar",
+        title="Query string", 
+        description="to search items", 
+        alias='item-query',
+        pattern="^fixedquery$",
+        deprecated=True,
+            ),
+        ] = None
+    ):
+    query_items = {"q": q}
+    return query_items
+
+# http://localhost:8000/items/?q=foo&q=bar (replace items with items03)
+# they can have defaults as well
+# http://localhost:8000/items03/
+# they can have title as well
