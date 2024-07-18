4144c46f8513f5021dc134e6a9650d05f5ea5bed
ark948 <armankarimi948@gmail.com>
2024-07-18 20:14:52 +0330

    Body - Feilds

---
+++ main05.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300 # declare field
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero") # deckare field
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+# https://fastapi.tiangolo.com/tutorial/body-fields/
+# Body - Fields
